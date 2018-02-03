e22f0cae0ddec32a263feddfcd64f17f00a45d9a
Luke Chang <lukejchang@gmail.com>
2018-02-03 15:26:37 -0500

    BUG: bug fixes to get tests working

--- feat/__init__.py
+++ feat/__init__.py
@@ -6,11 +6,6 @@ from __future__ import absolute_import
 
 __author__ = """Jin Hyun Cheong, Nathaniel Hanes, Luke Chang """
 __email__ = 'jcheong0428@gmail.com'
-version = {}
-with open("feat/version.py") as f:
-    exec(f.read(), version)
-__version__ = version['__version__'],
-
 __all__ = ['data',]
 
 from .data import Fex
