8aa20476ef37c200fde9ae0ccf94985e207f56e4
Sophie Byrne <sophie.k.byrne.20@dartmouth.edu>
2018-08-02 14:36:01 -0400

    took out print statements

--- feat/data.py
+++ feat/data.py
@@ -817,7 +817,6 @@ class Openface(Fex):
             linestyle: matplotlib linestyle
             gaze: (bool) whether to draw gaze based on data
         """
-        print('here')
         feats = ['AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r',
        'AU07_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r',
        'AU17_r', 'AU20_r', 'AU23_r', 'AU25_r', 'AU26_r', 'AU45_r']
