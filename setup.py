d301bdbe30a4f34942a248d260002b74ec4ed857
jcheong <jcheong0428@gmail.com>
2020-12-15 00:25:42 -0500

    updated setup script to download model from release

--- setup.py
+++ setup.py
@@ -43,3 +43,28 @@ setup(
     test_suite='feat/tests',
     **extra_setuptools_args
 )
+
+def _post_install():
+    from feat.utils import get_resource_path
+    import wget
+    import os
+
+    print("Downloading FEX emotion model.")
+    fex_emotion_model = "https://github.com/cosanlab/feat/releases/download/v0.1/fer_aug_model.h5"
+    wget.download(fex_emotion_model, get_resource_path())
+
+    if os.path.exists(os.path.join(get_resource_path(), "fer_aug_model.h5")):
+        print("\nFEX emotion model downloaded successfully.\n")
+    else:
+        print("Something went wrong. Model not found in directory.")
+
+    print("Downloading landmark detection model.")
+    lbfmodel = "https://github.com/cosanlab/feat/releases/download/v0.1/lbfmodel.yaml.txt"
+    wget.download(lbfmodel, get_resource_path())
+
+    if os.path.exists(os.path.join(get_resource_path(), "lbfmodel.yaml.txt")):
+        print("\nLandmark detection model downloaded successfully.\n")
+    else:
+        print("Something went wrong. Model not found in directory.")
+
+_post_install()
