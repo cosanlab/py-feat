691167c141aa5a3731294d05983c4fb6f8662ef2
ssxxzhouxi@gmail.com <ssxxzhouxi@gmail.com>
2021-01-26 14:45:45 -0500

    Added multiple methods

--- feat/tests/test_detector.py
+++ feat/tests/test_detector.py
@@ -11,7 +11,8 @@ import pandas as pd
 import feat
 import os
 import wget
-
+import cv2
+import numpy as np
 # def test_models():
 #     print("Downloading FEX emotion model.")
 #     fex_emotion_model = "https://github.com/cosanlab/feat/releases/download/v0.1/fer_aug_model.h5"
@@ -40,29 +41,108 @@ import wget
 #     landmark_model_path = os.path.join(get_resource_path(), landmark_model)
 #     assert os.path.exists(landmark_model_path)==True
 
+
 def test_detector():
+
     detector = Detector(n_jobs=1)
-    assert detector['n_jobs']==1
-    assert type(detector)==Detector
+    assert detector['n_jobs'] == 1
+    assert type(detector) == Detector
+
+    # Face Detector Test Case:
+    detector01 = Detector(face_model='FaceBoxes', landmark_model=None,
+                          au_occur_model=None, emotion_model=None, n_jobs=1)
+    inputFname = os.path.join(get_test_data_path(), "sampler0000.jpg")
+    img01 = cv2.imread(inputFname)
+    h, w, _ = img01.shape
+    out = detector01.face_detect(img01)
+    bbox_left = out[0][0]
+    bbox_right = out[0][1]
+    bbox_top = out[0][2]
+    bbox_bottom = out[0][3]
+    assert len(out[0]) == 4
+    assert bbox_left > 0 and bbox_right > 0 and bbox_top > 0 and bbox_bottom > 0 and \
+        bbox_left < bbox_right and bbox_top < bbox_bottom and bbox_left < w and \
+        bbox_right < w and bbox_top < h and bbox_bottom < h
+
+    detector02 = Detector(face_model='Retinaface', landmark_model=None,
+                          au_occur_model=None, emotion_model=None, n_jobs=1)
+    out = detector02.face_detect(img01)
+    bbox_left = out[0][0]
+    bbox_right = out[0][1]
+    bbox_top = out[0][2]
+    bbox_bottom = out[0][3]
+    assert len(out[0]) == 4
+    assert bbox_left > 0 and bbox_right > 0 and bbox_top > 0 and bbox_bottom > 0 and \
+        bbox_left < bbox_right and bbox_top < bbox_bottom and bbox_left < w and \
+        bbox_right < w and bbox_top < h and bbox_bottom < h
+
+    detector03 = Detector(face_model='MTCNN', landmark_model=None,
+                          au_occur_model=None, emotion_model=None, n_jobs=1)
+    out = detector03.face_detect(img01)
+    bbox_left = out[0][0]
+    bbox_right = out[0][1]
+    bbox_top = out[0][2]
+    bbox_bottom = out[0][3]
+    assert len(out[0]) == 4
+    assert bbox_left > 0 and bbox_right > 0 and bbox_top > 0 and bbox_bottom > 0 and \
+        bbox_left < bbox_right and bbox_top < bbox_bottom and bbox_left < w and \
+        bbox_right < w and bbox_top < h and bbox_bottom < h
+
+    # Landmark Detector Test Case:
+    inputFname = os.path.join(get_test_data_path(), "sampler0000.jpg")
+    img01 = cv2.imread(inputFname)
+    h, w, _ = img01.shape
+    detector01 = Detector(face_model='RetinaFace',
+                          emotion_model=None, landmark_model="MobileFaceNet")
+    bboxes = detector01.face_detect(img01)
+    landmarks = detector01.landmark_detect(img01, bboxes)
+    assert landmarks[0].shape == (68, 2)
+    assert np.any(landmarks[0][:, 0] > 0) and np.any(landmarks[0][:, 0] < w) and \
+        np.any(landmarks[0][:, 1] > 0) and np.any(landmarks[0][:, 1] < h)
+
+    detector02 = Detector(face_model='RetinaFace',
+                          emotion_model=None, landmark_model="MobileNet")
+    bboxes = detector02.face_detect(img01)
+    landmarks = detector02.landmark_detect(img01, bboxes)
+    assert landmarks[0].shape == (68, 2)
+    assert np.any(landmarks[0][:, 0] > 0) and np.any(landmarks[0][:, 0] < w) and \
+        np.any(landmarks[0][:, 1] > 0) and np.any(landmarks[0][:, 1] < h)
+
+    detector03 = Detector(face_model='RetinaFace',
+                          emotion_model=None, landmark_model="PFLD")
+    bboxes = detector03.face_detect(img01)
+    landmarks = detector03.landmark_detect(img01, bboxes)
+    assert landmarks[0].shape == (68, 2)
+    assert np.any(landmarks[0][:, 0] > 0) and np.any(landmarks[0][:, 0] < w) and \
+        np.any(landmarks[0][:, 1] > 0) and np.any(landmarks[0][:, 1] < h)
+
+    # AU Detection Case:
+    inputFname = os.path.join(get_test_data_path(), "sampler0000.jpg")
+    img01 = cv2.imread(inputFname)
+    detector1 = Detector(face_model='RetinaFace',emotion_model=None, landmark_model = "MobileFaceNet", au_occur_model="jaanet")
+    bboxes = detector1.face_detect(img01)
+    lands = detector1.landmark_detect(img01,bboxes)
+    aus = detector1.au_occur_detect(img01,lands)
+    assert aus.shape[-1] == 12
 
     # Test detect image
     inputFname = os.path.join(get_test_data_path(), "input.jpg")
-    out = detector.detect_image(inputFname = inputFname)
+    out = detector.detect_image(inputFname=inputFname)
     assert type(out) == Fex
     assert len(out) == 1
-    assert out.happiness.values[0] > 0 
+    assert out.happiness.values[0] > 0
 
     outputFname = os.path.join(get_test_data_path(), "output.csv")
     out = detector.detect_image(inputFname=inputFname, outputFname=outputFname)
     assert out
     assert os.path.exists(outputFname)
     out = pd.read_csv(outputFname)
-    assert out.happiness.values[0] > 0 
+    assert out.happiness.values[0] > 0
 
     # Test detect video
     inputFname = os.path.join(get_test_data_path(), "input.mp4")
     out = detector.detect_video(inputFname=inputFname)
-    assert len(out)==72
+    assert len(out) == 72
 
     outputFname = os.path.join(get_test_data_path(), "output.csv")
     out = detector.detect_video(inputFname=inputFname, outputFname=outputFname)
