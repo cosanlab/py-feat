c0c860752225df466ae13405778b54541379a401
Jin Cheong <jcheong0428@gmail.com>
2021-02-14 00:39:28 -0500

    remove unnecessary files

--- feat/au_detectors/DRML/lr_scheduler.py
+++
@@ -1,9 +0,0 @@
-# https://github.com/ZhiwenShao/PyTorch-JAANet/blob/master/lr_schedule.py
-
-def step_lr_scheduler(param_lr, optimizer, iter_num, gamma, stepsize, init_lr = 0.001):
-    lr = init_lr * (gamma ** (iter_num // stepsize))
-
-    for i, param_group in enumerate(optimizer.param_groups):
-        param_group['lr'] = lr * param_lr[i]
-    
-    return optimizer
