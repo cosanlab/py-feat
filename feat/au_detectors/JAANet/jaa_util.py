cc1f6ca35c092dcf63990125a97f25a5d6a66c54
Jin Cheong <jcheong0428@gmail.com>
2021-02-24 15:08:25 -0500

    black style code

--- feat/au_detectors/JAANet/jaa_util.py
+++ feat/au_detectors/JAANet/jaa_util.py
@@ -7,25 +7,37 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import math
 
+
 def str2bool(v):
-    return v.lower() in ('true')
+    return v.lower() in ("true")
+
 
 def tensor2img(img):
     img = img.data.numpy()
     if img.shape[0] == 1:
         img = np.tile(img, (3, 1, 1))
-    img = (np.transpose(img, (1, 2, 0))+ 1) / 2.0 * 255.0
+    img = (np.transpose(img, (1, 2, 0)) + 1) / 2.0 * 255.0
     return img.astype(np.uint8)
+
 
 def save_img(img, name, path):
     img = tensor2img(img)
     img = Image.fromarray(img)
-    img.save(path + name + '.png')
+    img.save(path + name + ".png")
     return img
 
 
-def AU_detection_evalv1(loader, region_learning, align_net, local_attention_refine,
-                local_au_net, global_au_feat, au_net, use_gpu=True, fail_threshold=0.1):
+def AU_detection_evalv1(
+    loader,
+    region_learning,
+    align_net,
+    local_attention_refine,
+    local_au_net,
+    global_au_feat,
+    au_net,
+    use_gpu=True,
+    fail_threshold=0.1,
+):
     missing_label = 9
     for i, batch in enumerate(loader):
         input, land, biocular, au = batch
@@ -39,9 +51,11 @@ def AU_detection_evalv1(loader, region_learning, align_net, local_attention_refi
         output_aus_map = local_attention_refine(aus_map.detach())
         local_au_out_feat = local_au_net(region_feat, output_aus_map)
         global_au_out_feat = global_au_feat(region_feat)
-        concat_au_feat = torch.cat((align_feat, global_au_out_feat, local_au_out_feat), 1)
+        concat_au_feat = torch.cat(
+            (align_feat, global_au_out_feat, local_au_out_feat), 1
+        )
         aus_output = au_net(concat_au_feat)
-        aus_output = (aus_output[:,1,:]).exp()
+        aus_output = (aus_output[:, 1, :]).exp()
 
         if i == 0:
             all_output = aus_output.data.cpu().float()
@@ -51,7 +65,9 @@ def AU_detection_evalv1(loader, region_learning, align_net, local_attention_refi
         else:
             all_output = torch.cat((all_output, aus_output.data.cpu().float()), 0)
             all_au = torch.cat((all_au, au.data.cpu().float()), 0)
-            all_pred_land = torch.cat((all_pred_land, align_output.data.cpu().float()), 0)
+            all_pred_land = torch.cat(
+                (all_pred_land, align_output.data.cpu().float()), 0
+            )
             all_land = torch.cat((all_land, land.data.cpu().float()), 0)
 
     AUoccur_pred_prob = all_output.data.numpy()
@@ -83,21 +99,28 @@ def AU_detection_evalv1(loader, region_learning, align_net, local_attention_refi
     errors = np.zeros((GT_land.shape[0], int(GT_land.shape[1] / 2)))
     mean_errors = np.zeros(GT_land.shape[0])
     for i in range(GT_land.shape[0]):
-        left_eye_x = GT_land[i, (20 - 1) * 2:(26 - 1) * 2:2]
+        left_eye_x = GT_land[i, (20 - 1) * 2 : (26 - 1) * 2 : 2]
         l_ocular_x = left_eye_x.mean()
-        left_eye_y = GT_land[i, (20 - 1) * 2 + 1:(26 - 1) * 2 + 1:2]
+        left_eye_y = GT_land[i, (20 - 1) * 2 + 1 : (26 - 1) * 2 + 1 : 2]
         l_ocular_y = left_eye_y.mean()
 
-        right_eye_x = GT_land[i, (26 - 1) * 2:(32 - 1) * 2:2]
+        right_eye_x = GT_land[i, (26 - 1) * 2 : (32 - 1) * 2 : 2]
         r_ocular_x = right_eye_x.mean()
-        right_eye_y = GT_land[i, (26 - 1) * 2 + 1:(32 - 1) * 2 + 1:2]
+        right_eye_y = GT_land[i, (26 - 1) * 2 + 1 : (32 - 1) * 2 + 1 : 2]
         r_ocular_y = right_eye_y.mean()
 
-        biocular = math.sqrt((l_ocular_x - r_ocular_x) ** 2 + (l_ocular_y - r_ocular_y) ** 2)
+        biocular = math.sqrt(
+            (l_ocular_x - r_ocular_x) ** 2 + (l_ocular_y - r_ocular_y) ** 2
+        )
 
         for j in range(0, GT_land.shape[1], 2):
-            errors[i, int(j / 2)] = math.sqrt((GT_land[i, j] - pred_land[i, j]) ** 2 + (
-                    GT_land[i, j + 1] - pred_land[i, j + 1]) ** 2) / biocular
+            errors[i, int(j / 2)] = (
+                math.sqrt(
+                    (GT_land[i, j] - pred_land[i, j]) ** 2
+                    + (GT_land[i, j + 1] - pred_land[i, j + 1]) ** 2
+                )
+                / biocular
+            )
 
         mean_errors[i] = errors[i].mean()
     mean_error = mean_errors.mean()
@@ -109,11 +132,20 @@ def AU_detection_evalv1(loader, region_learning, align_net, local_attention_refi
     return f1score_arr, acc_arr, mean_error, failure_rate
 
 
-def AU_detection_evalv2(loader, region_learning, align_net, local_attention_refine,
-                local_au_net, global_au_feat, au_net, use_gpu=True, fail_threshold = 0.1):
+def AU_detection_evalv2(
+    loader,
+    region_learning,
+    align_net,
+    local_attention_refine,
+    local_au_net,
+    global_au_feat,
+    au_net,
+    use_gpu=True,
+    fail_threshold=0.1,
+):
     missing_label = 9
     for i, batch in enumerate(loader):
-        input, land, biocular, au  = batch
+        input, land, biocular, au = batch
         if use_gpu:
             input, land, au = input.cuda(), land.cuda(), au.cuda()
 
@@ -125,9 +157,11 @@ def AU_detection_evalv2(loader, region_learning, align_net, local_attention_refi
         local_au_out_feat, local_aus_output = local_au_net(region_feat, output_aus_map)
         local_aus_output = (local_aus_output[:, 1, :]).exp()
         global_au_out_feat = global_au_feat(region_feat)
-        concat_au_feat = torch.cat((align_feat, global_au_out_feat, local_au_out_feat.detach()), 1)
+        concat_au_feat = torch.cat(
+            (align_feat, global_au_out_feat, local_au_out_feat.detach()), 1
+        )
         aus_output = au_net(concat_au_feat)
-        aus_output = (aus_output[:,1,:]).exp()
+        aus_output = (aus_output[:, 1, :]).exp()
 
         if i == 0:
             all_local_output = local_aus_output.data.cpu().float()
@@ -136,10 +170,14 @@ def AU_detection_evalv2(loader, region_learning, align_net, local_attention_refi
             all_pred_land = align_output.data.cpu().float()
             all_land = land.data.cpu().float()
         else:
-            all_local_output = torch.cat((all_local_output, local_aus_output.data.cpu().float()), 0)
+            all_local_output = torch.cat(
+                (all_local_output, local_aus_output.data.cpu().float()), 0
+            )
             all_output = torch.cat((all_output, aus_output.data.cpu().float()), 0)
             all_au = torch.cat((all_au, au.data.cpu().float()), 0)
-            all_pred_land = torch.cat((all_pred_land, align_output.data.cpu().float()), 0)
+            all_pred_land = torch.cat(
+                (all_pred_land, align_output.data.cpu().float()), 0
+            )
             all_land = torch.cat((all_land, land.data.cpu().float()), 0)
 
     AUoccur_pred_prob = all_output.data.numpy()
@@ -148,8 +186,9 @@ def AU_detection_evalv2(loader, region_learning, align_net, local_attention_refi
     pred_land = all_pred_land.data.numpy()
     GT_land = all_land.data.numpy()
     # np.savetxt('BP4D_part1_pred_land_49.txt', pred_land, fmt='%.4f', delimiter='\t')
-    np.savetxt('B3D_val_predAUprob-2_all_.txt', AUoccur_pred_prob, fmt='%f',
-               delimiter='\t')
+    np.savetxt(
+        "B3D_val_predAUprob-2_all_.txt", AUoccur_pred_prob, fmt="%f", delimiter="\t"
+    )
     # AUs
     AUoccur_pred = np.zeros(AUoccur_pred_prob.shape)
     AUoccur_pred[AUoccur_pred_prob < 0.5] = 0
@@ -184,21 +223,28 @@ def AU_detection_evalv2(loader, region_learning, align_net, local_attention_refi
     errors = np.zeros((GT_land.shape[0], int(GT_land.shape[1] / 2)))
     mean_errors = np.zeros(GT_land.shape[0])
     for i in range(GT_land.shape[0]):
-        left_eye_x = GT_land[i, (20 - 1) * 2:(26 - 1) * 2:2]
+        left_eye_x = GT_land[i, (20 - 1) * 2 : (26 - 1) * 2 : 2]
         l_ocular_x = left_eye_x.mean()
-        left_eye_y = GT_land[i, (20 - 1) * 2 + 1:(26 - 1) * 2 + 1:2]
+        left_eye_y = GT_land[i, (20 - 1) * 2 + 1 : (26 - 1) * 2 + 1 : 2]
         l_ocular_y = left_eye_y.mean()
 
-        right_eye_x = GT_land[i, (26 - 1) * 2:(32 - 1) * 2:2]
+        right_eye_x = GT_land[i, (26 - 1) * 2 : (32 - 1) * 2 : 2]
         r_ocular_x = right_eye_x.mean()
-        right_eye_y = GT_land[i, (26 - 1) * 2 + 1:(32 - 1) * 2 + 1:2]
+        right_eye_y = GT_land[i, (26 - 1) * 2 + 1 : (32 - 1) * 2 + 1 : 2]
         r_ocular_y = right_eye_y.mean()
 
-        biocular = math.sqrt((l_ocular_x - r_ocular_x) ** 2 + (l_ocular_y - r_ocular_y) ** 2)
+        biocular = math.sqrt(
+            (l_ocular_x - r_ocular_x) ** 2 + (l_ocular_y - r_ocular_y) ** 2
+        )
 
         for j in range(0, GT_land.shape[1], 2):
-            errors[i, int(j / 2)] = math.sqrt((GT_land[i, j] - pred_land[i, j]) ** 2 + (
-                    GT_land[i, j + 1] - pred_land[i, j + 1]) ** 2) / biocular
+            errors[i, int(j / 2)] = (
+                math.sqrt(
+                    (GT_land[i, j] - pred_land[i, j]) ** 2
+                    + (GT_land[i, j + 1] - pred_land[i, j + 1]) ** 2
+                )
+                / biocular
+            )
 
         mean_errors[i] = errors[i].mean()
     mean_error = mean_errors.mean()
@@ -207,10 +253,27 @@ def AU_detection_evalv2(loader, region_learning, align_net, local_attention_refi
     failure_ind[mean_errors > fail_threshold] = 1
     failure_rate = failure_ind.sum() / failure_ind.shape[0]
 
-    return local_f1score_arr, local_acc_arr, f1score_arr, acc_arr, mean_error, failure_rate
+    return (
+        local_f1score_arr,
+        local_acc_arr,
+        f1score_arr,
+        acc_arr,
+        mean_error,
+        failure_rate,
+    )
 
 
-def vis_attention(loader, region_learning, align_net, local_attention_refine, write_path_prefix, net_name, epoch, alpha = 0.5, use_gpu=True):
+def vis_attention(
+    loader,
+    region_learning,
+    align_net,
+    local_attention_refine,
+    write_path_prefix,
+    net_name,
+    epoch,
+    alpha=0.5,
+    use_gpu=True,
+):
     for i, batch in enumerate(loader):
         input, land, biocular, au = batch
         # if i > 1:
@@ -224,41 +287,88 @@ def vis_attention(loader, region_learning, align_net, local_attention_refine, wr
         output_aus_map = local_attention_refine(aus_map.detach())
 
         # aus_map is predefined, and output_aus_map is refined
-        spatial_attention = output_aus_map #aus_map
+        spatial_attention = output_aus_map  # aus_map
         if i == 0:
             all_input = input.data.cpu().float()
             all_spatial_attention = spatial_attention.data.cpu().float()
         else:
             all_input = torch.cat((all_input, input.data.cpu().float()), 0)
-            all_spatial_attention = torch.cat((all_spatial_attention, spatial_attention.data.cpu().float()), 0)
+            all_spatial_attention = torch.cat(
+                (all_spatial_attention, spatial_attention.data.cpu().float()), 0
+            )
 
     for i in range(all_spatial_attention.shape[0]):
-        background = save_img(all_input[i], 'input', write_path_prefix + net_name + '/vis_map/' + str(epoch) +
-                        '/' + str(i) + '_')
+        background = save_img(
+            all_input[i],
+            "input",
+            write_path_prefix
+            + net_name
+            + "/vis_map/"
+            + str(epoch)
+            + "/"
+            + str(i)
+            + "_",
+        )
         for j in range(all_spatial_attention.shape[1]):
             fig, ax = plt.subplots()
             # print(all_spatial_attention[i,j].max(), all_spatial_attention[i,j].min())
             # cax = ax.imshow(all_spatial_attention[i,j], cmap='jet', interpolation='bicubic')
-            cax = ax.imshow(all_spatial_attention[i, j], cmap='jet', interpolation='bicubic', vmin=0, vmax=1)
-            ax.axis('off')
+            cax = ax.imshow(
+                all_spatial_attention[i, j],
+                cmap="jet",
+                interpolation="bicubic",
+                vmin=0,
+                vmax=1,
+            )
+            ax.axis("off")
             ax.axes.get_xaxis().set_visible(False)
             ax.axes.get_yaxis().set_visible(False)
             #        cbar = fig.colorbar(cax)
-            fig.savefig(write_path_prefix + net_name + '/vis_map/' + str(epoch) +
-                        '/' + str(i) + '_au_' + str(j) + '.png', bbox_inches='tight', pad_inches=0)
+            fig.savefig(
+                write_path_prefix
+                + net_name
+                + "/vis_map/"
+                + str(epoch)
+                + "/"
+                + str(i)
+                + "_au_"
+                + str(j)
+                + ".png",
+                bbox_inches="tight",
+                pad_inches=0,
+            )
 
         for j in range(all_spatial_attention.shape[1]):
-            overlay = Image.open(write_path_prefix + net_name + '/vis_map/' + str(epoch) +
-                        '/' + str(i) + '_au_' + str(j) + '.png')
+            overlay = Image.open(
+                write_path_prefix
+                + net_name
+                + "/vis_map/"
+                + str(epoch)
+                + "/"
+                + str(i)
+                + "_au_"
+                + str(j)
+                + ".png"
+            )
             overlay = overlay.resize(background.size, Image.ANTIALIAS)
-            background = background.convert('RGBA')
-            overlay = overlay.convert('RGBA')
+            background = background.convert("RGBA")
+            overlay = overlay.convert("RGBA")
             new_img = Image.blend(background, overlay, alpha)
-            new_img.save(write_path_prefix + net_name + '/overlay_vis_map/' + str(epoch) +
-                        '/' + str(i) + '_au_' + str(j) + '.png', 'PNG')
+            new_img.save(
+                write_path_prefix
+                + net_name
+                + "/overlay_vis_map/"
+                + str(epoch)
+                + "/"
+                + str(i)
+                + "_au_"
+                + str(j)
+                + ".png",
+                "PNG",
+            )
 
 
-def dice_loss(pred, target, smooth = 1):
+def dice_loss(pred, target, smooth=1):
     """This definition generalize to real valued pred and target vector.
     This should be differentiable.
     pred: tensor with first dimension as batch
@@ -273,21 +383,23 @@ def dice_loss(pred, target, smooth = 1):
     A_sum = torch.sum(iflat * iflat)
     B_sum = torch.sum(tflat * tflat)
 
-    return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth)) / iflat.size(0)
+    return 1 - ((2.0 * intersection + smooth) / (A_sum + B_sum + smooth)) / iflat.size(
+        0
+    )
 
 
 def au_softmax_loss(input, target, weight=None, size_average=True, reduce=True):
-    classify_loss = nn.NLLLoss(size_average=size_average, reduce=reduce, ignore_index = 9)
+    classify_loss = nn.NLLLoss(size_average=size_average, reduce=reduce, ignore_index=9)
 
     for i in range(input.size(2)):
-        
+
         t_input = input[:, :, i]
         t_target = target[:, i]
-        #print("t_input shape",t_input.shape)
-        #print("t_target shape",t_target.shape)
-        
+        # print("t_input shape",t_input.shape)
+        # print("t_target shape",t_target.shape)
+
         t_loss = classify_loss(t_input, t_target)
-        #print('wt:',weight[i])
+        # print('wt:',weight[i])
         if weight is not None:
             t_loss = t_loss * weight[i]
         t_loss = torch.unsqueeze(t_loss, 0)
@@ -302,7 +414,7 @@ def au_softmax_loss(input, target, weight=None, size_average=True, reduce=True):
         return loss.sum()
 
 
-def au_dice_loss(input, target, weight=None, smooth = 1, size_average=True):
+def au_dice_loss(input, target, weight=None, smooth=1, size_average=True):
     for i in range(input.size(2)):
         # input is log_softmax, t_input is probability
         t_input = (input[:, 1, i]).exp()
@@ -326,16 +438,16 @@ def au_dice_loss(input, target, weight=None, smooth = 1, size_average=True):
 
 def landmark_loss(input, target, biocular, size_average=True):
     for i in range(input.size(0)):
-        
-        t_input = input[i,:]
-        t_target = target[i,:]
-        #print(t_input)
-        #print(t_target)
-        #print("difference:", (t_input - t_target) ** 2)
-        #print("biocular:",(2.0*biocular[i]))
-        #print("t_loss:",t_loss)
-        
-        t_loss = torch.sum((t_input - t_target) ** 2) / (2.0*biocular[i])
+
+        t_input = input[i, :]
+        t_target = target[i, :]
+        # print(t_input)
+        # print(t_target)
+        # print("difference:", (t_input - t_target) ** 2)
+        # print("biocular:",(2.0*biocular[i]))
+        # print("t_loss:",t_loss)
+
+        t_loss = torch.sum((t_input - t_target) ** 2) / (2.0 * biocular[i])
         t_loss = torch.unsqueeze(t_loss, 0)
         if i == 0:
             loss = t_loss
@@ -367,25 +479,29 @@ def attention_refine_loss(input, target, size_average=True, reduce=True):
             loss = torch.cat((loss, t_loss), 0)
     # sum losses of all AUs
     return loss.sum()
-    
+
+
 def calculate_AU_weight(master_dataframe):
     """
-    Calculates the AU weight according to a occurence dataframe 
-    inputs: 
+    Calculates the AU weight according to a occurence dataframe
+    inputs:
         occurence_df: a pandas dataframe containing occurence of each AU. See BP4D+
     """
-    #occurence_df = occurence_df.rename(columns = {'two':'new_name'})
-    occurence_df = master_dataframe[["1","2","4","6","7","10","12","14","15","17","23","24"]]
+    # occurence_df = occurence_df.rename(columns = {'two':'new_name'})
+    occurence_df = master_dataframe[
+        ["1", "2", "4", "6", "7", "10", "12", "14", "15", "17", "23", "24"]
+    ]
 
     weight_mtrx = np.zeros((occurence_df.shape[1], 1))
     for i in range(occurence_df.shape[1]):
-        weight_mtrx[i] = np.sum(occurence_df.iloc[:, i]
-                                == 1) / float(occurence_df.shape[0])
-    weight_mtrx = 1.0/weight_mtrx
+        weight_mtrx[i] = np.sum(occurence_df.iloc[:, i] == 1) / float(
+            occurence_df.shape[0]
+        )
+    weight_mtrx = 1.0 / weight_mtrx
 
     print(weight_mtrx)
-    #weight_mtrx[weight_mtrx == np.inf] = 0
-    #print(np.sum(weight_mtrx)*len(weight_mtrx))
-    weight_mtrx = weight_mtrx / (np.sum(weight_mtrx)*weight_mtrx.shape[1])
+    # weight_mtrx[weight_mtrx == np.inf] = 0
+    # print(np.sum(weight_mtrx)*len(weight_mtrx))
+    weight_mtrx = weight_mtrx / (np.sum(weight_mtrx) * weight_mtrx.shape[1])
 
-    return(weight_mtrx)
+    return weight_mtrx
--- feat/tests/test_utils.py
+++ feat/tests/test_utils.py
@@ -9,35 +9,47 @@ from pandas import DataFrame, Series
 import numpy as np
 from os.path import join, exists
 from .utils import get_test_data_path
-from feat.utils import read_facet, read_openface, read_affectiva, registration, neutral, softmax, load_h5, load_pickled_model
+from feat.utils import (
+    read_facet,
+    read_openface,
+    read_affectiva,
+    registration,
+    neutral,
+    softmax,
+    load_h5,
+    load_pickled_model,
+)
 from nltools.data import Adjacency
 import unittest
 
+
 def test_utils():
-    sample = read_openface(join(get_test_data_path(), 'OpenFace_Test.csv'))
-    lm_cols = ['x_'+str(i) for i in range(0,68)]+['y_'+str(i) for i in range(0,68)]
+    sample = read_openface(join(get_test_data_path(), "OpenFace_Test.csv"))
+    lm_cols = ["x_" + str(i) for i in range(0, 68)] + [
+        "y_" + str(i) for i in range(0, 68)
+    ]
     sample_face = np.array([sample[lm_cols].values[0]])
     registered_lm = registration(sample_face)
-    assert(registered_lm.shape==(1,136))
+    assert registered_lm.shape == (1, 136)
 
     with pytest.raises(ValueError):
-        registration(sample_face, method='badmethod')
+        registration(sample_face, method="badmethod")
     with pytest.raises(TypeError):
-        registration(sample_face, method = np.array([1,2,3,4]))
+        registration(sample_face, method=np.array([1, 2, 3, 4]))
     with pytest.raises(AssertionError):
         registration([sample_face[0]])
     with pytest.raises(AssertionError):
         registration(sample_face[0])
     with pytest.raises(AssertionError):
-        registration(sample_face[:,:-1])
+        registration(sample_face[:, :-1])
 
     # Test softmax
-    assert(softmax(0) == .5)
-    # Test badfile. 
+    assert softmax(0) == 0.5
+    # Test badfile.
     with pytest.raises(Exception):
         load_h5("badfile.h5")
 
-    # Test loading of pickled model 
+    # Test loading of pickled model
     out = load_pickled_model()
     with pytest.raises(Exception):
         load_pickled_model("badfile.pkl")
