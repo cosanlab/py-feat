96047748aa7f01770f9f0a0b62e5024ea43f7c99
ssxxzhouxi@gmail.com <ssxxzhouxi@gmail.com>
2021-03-23 03:38:42 -0400

    add PCA downloads

--- setup.py
+++ setup.py
@@ -239,6 +239,15 @@ class CustomInstall(install):
                     wget.download(logmodel, get_resource_path(), bar=None)
                 except:
                     print("Logistic Action Unit model failed to download.")  
+                    
+            if os.path.exists(os.path.join(get_resource_path(), "hog_pca_all_emotio.joblib")):
+                print("\nPCA model downloaded successfully.\n")
+            else:
+                try:
+                    pcamodel = "https://github.com/cosanlab/feat/releases/download/v0.1/hog_pca_all_emotio.joblib"
+                    wget.download(pcamodel, get_resource_path(), bar=None)
+                except:
+                    print("PCA model failed to download.")  
 
         atexit.register(_post_install)
         install.run(self)
