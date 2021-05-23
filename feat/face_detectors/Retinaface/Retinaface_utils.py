37ec48bc5bf176bc7a92ca8171f4ed5a96a1a3b1
Tiankang Xie <tiankang@liquidswords.dartmouth.edu>
2021-05-23 12:40:35 -0400

    batch process script

--- feat/face_detectors/Retinaface/Retinaface_utils.py
+++ feat/face_detectors/Retinaface/Retinaface_utils.py
@@ -14,7 +14,7 @@ def decode(loc, priors, variances):
     Return:
         decoded bounding box predictions
     """
-
+    
     boxes = torch.cat(
         (
             priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:],
