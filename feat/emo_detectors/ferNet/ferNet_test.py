b22805b432b06b10b477c1be31435ca8e6f6c2f4
Tiankang Xie <tiankang@liquidswords.dartmouth.edu>
2021-05-24 13:36:02 -0400

    fix fer model

--- feat/emo_detectors/ferNet/ferNet_test.py
+++ feat/emo_detectors/ferNet/ferNet_test.py
@@ -122,7 +122,7 @@ class ferNetModule(nn.Module):
         img_transforms = transforms.Compose(
             [
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                #transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
             ]
         )
 
