0b33daef08bb342c34ad5a211b461bc1dad75415
ejolly <eshin.jolly@gmail.com>
2022-03-11 13:35:25 -0500

    Add Eshin to license, update docs CI badge. Version bump.

--- feat/__init__.py
+++ feat/__init__.py
@@ -4,8 +4,8 @@
 
 from __future__ import absolute_import
 
-__author__ = """Jin Hyun Cheong, Tiankang Xie, Sophie Byrne, Luke Chang """
-__email__ = "jcheong0428@gmail.com"
+__author__ = """Jin Hyun Cheong, Tiankang Xie, Sophie Byrne, Eshin Jolly, Luke Chang """
+__email__ = "jcheong0428@gmail.com, eshin.jolly@gmail.com"
 __all__ = ["detector", "data", "utils", "plotting", "__version__"]
 
 from .data import Fex
