02adceafabd7d7802f094ef3003e3419b7e92fcd
ssxxzhouxi@gmail.com <ssxxzhouxi@gmail.com>
2022-03-29 16:22:23 -0400

    fix drml test to allow 4d & 3d input

--- feat/au_detectors/DRML/DRML_test.py
+++ feat/au_detectors/DRML/DRML_test.py
@@ -12,7 +12,7 @@ class DRMLNet(nn.Module):
         """
         Initialize.
         """
-        super(DRMLNet,self).__init__()
+        super(DRMLNet, self).__init__()
 
         self.params = {
             "config_au_num": 12,
@@ -23,11 +23,13 @@ class DRMLNet(nn.Module):
         self.use_gpu = torch.cuda.is_available()
         self.drml_net = DRML_net(AU_num=self.params["config_au_num"])
         if self.use_gpu:
-            self.drml_net.load_state_dict(torch.load(self.params["config_write_path_prefix"]))
+            self.drml_net.load_state_dict(torch.load(
+                self.params["config_write_path_prefix"]))
             self.drml_net = self.drml_net.cuda()
         else:
             self.drml_net.load_state_dict(
-                torch.load(self.params["config_write_path_prefix"], map_location={"cuda:0": "cpu"})
+                torch.load(self.params["config_write_path_prefix"], map_location={
+                           "cuda:0": "cpu"})
             )
         self.drml_net.eval()
 
@@ -35,7 +37,7 @@ class DRMLNet(nn.Module):
         """
         Wrapper function that takes in imgs and produces AU occurence predictions.
         Args:
-            imgs: processed images type numpy array
+            imgs: processed images type 4d numpy array (BATCH, )
         Return:
             all_pred_au: AU occurence predictions for all AU classes
         """
@@ -44,21 +46,29 @@ class DRMLNet(nn.Module):
             [
                 transforms.CenterCrop(170),
                 transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                transforms.Normalize([0.485, 0.456, 0.406], [
+                                     0.229, 0.224, 0.225]),
             ]
+
         )
-        imgs = Image.fromarray(imgs)
-        input = img_transforms(imgs)
-        if len(input.shape) < 4:
+        if len(imgs.shape) < 4:
+            imgs = np.expand_dims(imgs, 0)
+
+        img_concat_tensor = None
+
+        for batch in imgs.shape[0]:
+            img = Image.fromarray(imgs[batch])
+            input = img_transforms(imgs)
             input.unsqueeze_(0)
+            if img_concat_tensor is None:
+                img_concat_tensor = input
+            else:
+                img_concat_tensor = torch.cat((img_concat_tensor, input), 0)
 
         if self.use_gpu:
-            input = input.cuda()
+            img_concat_tensor = img_concat_tensor.cuda()
 
-        pred_au = self.drml_net(input)
+        pred_au = self.drml_net(img_concat_tensor)
         all_pred_au = pred_au.data.cpu().float()
-        # all_pred_au = (all_pred_au[:, 1, :]).exp()
-        # all_pred_au[all_pred_au < 0.5] = 0
-        # all_pred_au[all_pred_au >= 0.5] = 1
         all_pred_au = all_pred_au.data.numpy()
         return all_pred_au
