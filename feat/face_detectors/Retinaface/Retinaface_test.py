3e96b8f4d46092a790e30c69c7faa7f603ea9125
ljchang <luke.j.chang@dartmouth.edu>
2022-07-19 17:56:11 -0400

    cleaned up unused code

--- feat/face_detectors/Retinaface/Retinaface_test.py
+++ feat/face_detectors/Retinaface/Retinaface_test.py
@@ -14,98 +14,6 @@ from feat.utils import (get_resource_path,
                         convert_image_to_tensor, 
                         set_torch_device)
 
-
-# some global configs
-# network = "mobile0.25"
-# trained_model = os.path.join(get_resource_path(), f"mobilenet0.25_Final.pth")
-# confidence_threshold = 0.05
-# top_k = 5000
-# keep_top_k = 750
-# nms_threshold = 0.3
-# vis_threshold = 0.5
-# resize = 1
-
-# # cpu = True
-# cfg_mnet = {
-#     "name": "mobilenet0.25",
-#     "min_sizes": [[16, 32], [64, 128], [256, 512]],
-#     "steps": [8, 16, 32],
-#     "variance": [0.1, 0.2],
-#     "clip": False,
-#     "loc_weight": 2.0,
-#     "gpu_train": True,
-#     "batch_size": 32,
-#     "ngpu": 1,
-#     "epoch": 250,
-#     "decay1": 190,
-#     "decay2": 220,
-#     "image_size": 640,
-#     "pretrain": False,
-#     "return_layers": {"stage1": 1, "stage2": 2, "stage3": 3},
-#     "in_channel": 32,
-#     "out_channel": 64,
-# }
-# cfg_re50 = {
-#     "name": "Resnet50",
-#     "min_sizes": [[16, 32], [64, 128], [256, 512]],
-#     "steps": [8, 16, 32],
-#     "variance": [0.1, 0.2],
-#     "clip": False,
-#     "loc_weight": 2.0,
-#     "gpu_train": True,
-#     "batch_size": 24,
-#     "ngpu": 4,
-#     "epoch": 100,
-#     "decay1": 70,
-#     "decay2": 90,
-#     "image_size": 840,
-#     "pretrain": True,
-#     "return_layers": {"layer2": 1, "layer3": 2, "layer4": 3},
-#     "in_channel": 256,
-#     "out_channel": 256,
-# }
-
-
-# def check_keys(model, pretrained_state_dict):
-#     ckpt_keys = set(pretrained_state_dict.keys())
-#     model_keys = set(model.state_dict().keys())
-#     used_pretrained_keys = model_keys & ckpt_keys
-#     unused_pretrained_keys = ckpt_keys - model_keys
-#     missing_keys = model_keys - ckpt_keys
-#     # print('Missing keys:{}'.format(len(missing_keys)))
-#     # print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
-#     # print('Used keys:{}'.format(len(used_pretrained_keys)))
-#     assert len(used_pretrained_keys) > 0, "load NONE from pretrained checkpoint"
-#     return True
-
-
-# def remove_prefix(state_dict, prefix):
-#     """Old style model is stored with all names of parameters sharing common prefix 'module.'"""
-#     # print('remove prefix \'{}\''.format(prefix))
-#     f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
-#     return {f(key): value for key, value in state_dict.items()}
-
-
-# def load_model(model, pretrained_path, load_to_cpu):
-#     # print("Loading pretrained model from {}".format(pretrained_path))
-#     if load_to_cpu:
-#         pretrained_dict = torch.load(
-#             pretrained_path, map_location=lambda storage, loc: storage
-#         )
-#     else:
-#         device = torch.cuda.current_device()
-#         pretrained_dict = torch.load(
-#             pretrained_path, map_location=lambda storage, loc: storage.cuda(device)
-#         )
-#     if "state_dict" in pretrained_dict.keys():
-#         pretrained_dict = remove_prefix(pretrained_dict["state_dict"], "module.")
-#     else:
-#         pretrained_dict = remove_prefix(pretrained_dict, "module.")
-#     check_keys(model, pretrained_dict)
-#     model.load_state_dict(pretrained_dict, strict=False)
-#     return model
-
-
 class Retinaface:
     def __init__(self, device='auto', timer_flag=False, resize=1, vis_threshold=0.5, nms_threshold=0.3, keep_top_k=750, top_k=5000, confidence_threshold=0.05):
         '''
@@ -146,7 +54,6 @@ class Retinaface:
         # net and model
         self.device = set_torch_device(device=device)
         net = RetinaFace(cfg=self.cfg, phase="test")
-        # pretrained_dict = torch.load(os.path.join(get_resource_path(), f"mobilenet0.25_Final.pth"), map_location=lambda storage, loc: storage)
         pretrained_dict = torch.load(os.path.join(get_resource_path(), f"mobilenet0.25_Final.pth"), map_location=self.device)
         net.load_state_dict(pretrained_dict, strict=False)            
         net = net.to(self.device)
