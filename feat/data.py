e76bc7f2c86bcfdff6a8d23eedf3c9876baa5ce2
ljchang <luke.j.chang@dartmouth.edu>
2022-08-02 18:59:11 -0400

    fixed bugs with multiface batch processing

--- feat/data.py
+++ feat/data.py
@@ -1608,189 +1608,6 @@ class Fex(DataFrame):
             all_figs.append(f)
         return all_figs
 
-    def inverse_transform(self, padding_left, padding_top, scale):
-        """Helper function to inverse the image transformations applied to the ImageData class for batch processing
-
-        Args:
-            padding_left (list, np.array, torch.tensor): amount of left padding applied to each image
-            padding_top (list, np.array, torch.tensor): amount of top padding applied to each image
-            sca;e (list, np.array, torch.tensor): amount of scaling applied to each image
-
-        Returns:
-            Fex Instance
-        """
-
-        def convert_to_array(input_variable):
-            if isinstance(input_variable, torch.Tensor):
-                output_variable = input_variable.numpy()
-            else:
-                output_variable = np.array(input_variable)
-            return output_variable
-
-        data = self.copy()
-        padding_left = convert_to_array(padding_left)
-        padding_top = convert_to_array(padding_top)
-        scale = convert_to_array(scale)
-
-        if (
-            len(data) != len(padding_left)
-            or len(data) != len(padding_top)
-            or len(data) != len(scale)
-        ):
-            raise ValueError("Data must be the same length as the transformation data.")
-
-        # Remove left side padding & Rescale
-        for variable in [
-            "FaceRectX",
-            "FaceRectWidth",
-            "x_0",
-            "x_1",
-            "x_2",
-            "x_3",
-            "x_4",
-            "x_5",
-            "x_6",
-            "x_7",
-            "x_8",
-            "x_9",
-            "x_10",
-            "x_11",
-            "x_12",
-            "x_13",
-            "x_14",
-            "x_15",
-            "x_16",
-            "x_17",
-            "x_18",
-            "x_19",
-            "x_20",
-            "x_21",
-            "x_22",
-            "x_23",
-            "x_24",
-            "x_25",
-            "x_26",
-            "x_27",
-            "x_28",
-            "x_29",
-            "x_30",
-            "x_31",
-            "x_32",
-            "x_33",
-            "x_34",
-            "x_35",
-            "x_36",
-            "x_37",
-            "x_38",
-            "x_39",
-            "x_40",
-            "x_41",
-            "x_42",
-            "x_43",
-            "x_44",
-            "x_45",
-            "x_46",
-            "x_47",
-            "x_48",
-            "x_49",
-            "x_50",
-            "x_51",
-            "x_52",
-            "x_53",
-            "x_54",
-            "x_55",
-            "x_56",
-            "x_57",
-            "x_58",
-            "x_59",
-            "x_60",
-            "x_61",
-            "x_62",
-            "x_63",
-            "x_64",
-            "x_65",
-            "x_66",
-            "x_67",
-        ]:
-            data[variable] = (data[variable] - padding_left) / scale
-
-        # Remove top side padding & Rescale
-        for variable in [
-            "FaceRectY",
-            "FaceRectHeight",
-            "y_0",
-            "y_1",
-            "y_2",
-            "y_3",
-            "y_4",
-            "y_5",
-            "y_6",
-            "y_7",
-            "y_8",
-            "y_9",
-            "y_10",
-            "y_11",
-            "y_12",
-            "y_13",
-            "y_14",
-            "y_15",
-            "y_16",
-            "y_17",
-            "y_18",
-            "y_19",
-            "y_20",
-            "y_21",
-            "y_22",
-            "y_23",
-            "y_24",
-            "y_25",
-            "y_26",
-            "y_27",
-            "y_28",
-            "y_29",
-            "y_30",
-            "y_31",
-            "y_32",
-            "y_33",
-            "y_34",
-            "y_35",
-            "y_36",
-            "y_37",
-            "y_38",
-            "y_39",
-            "y_40",
-            "y_41",
-            "y_42",
-            "y_43",
-            "y_44",
-            "y_45",
-            "y_46",
-            "y_47",
-            "y_48",
-            "y_49",
-            "y_50",
-            "y_51",
-            "y_52",
-            "y_53",
-            "y_54",
-            "y_55",
-            "y_56",
-            "y_57",
-            "y_58",
-            "y_59",
-            "y_60",
-            "y_61",
-            "y_62",
-            "y_63",
-            "y_64",
-            "y_65",
-            "y_66",
-            "y_67",
-        ]:
-            data[variable] = (data[variable] - padding_top) / scale
-
-        return data
-
 
 class Fextractor:
     """
@@ -2025,3 +1842,70 @@ class ImageDataset(Dataset):
                 "Scale": 1.0,
                 "Padding": {"Left": 0, "Top": 0, "Right": 0, "Bottom": 0},
             }
+
+
+def _inverse_face_transform(faces, batch_data):
+    """Helper function to invert the Image Data batch transforms on the face bounding boxes
+
+    Args:
+        faces (list): list of lists from face_detector
+        batch_data (dict): batch data from Image Data Class
+
+    Returns:
+        transformed list of lists
+    """
+
+    out_frame = []
+    for frame, left, top, scale in zip(
+        faces,
+        batch_data["Padding"]["Left"].numpy(),
+        batch_data["Padding"]["Top"].numpy(),
+        batch_data["Scale"].numpy(),
+    ):
+        out_face = []
+        for face in frame:
+            out_face.append(
+                np.append(
+                    (
+                        np.array(
+                            [
+                                face[0] - left,
+                                face[1] - top,
+                                face[2] - left,
+                                face[3] - top,
+                            ]
+                        )
+                        / scale
+                    ),
+                    face[4],
+                )
+            )
+        out_frame.append(out_face)
+    return out_frame
+
+
+def _inverse_landmark_transform(landmarks, batch_data):
+    """Helper function to invert the Image Data batch transforms on the facial landmarks
+
+    Args:
+        landmarks (list): list of lists from landmark_detector
+        batch_data (dict): batch data from Image Data Class
+
+    Returns:
+        transformed list of lists
+    """
+
+    out_frame = []
+    for frame, left, top, scale in zip(
+        landmarks,
+        batch_data["Padding"]["Left"].numpy(),
+        batch_data["Padding"]["Top"].numpy(),
+        batch_data["Scale"].numpy(),
+    ):
+        out_landmark = []
+        for landmark in frame:
+            out_landmark.append(
+                (landmark - np.ones(landmark.shape) * [left, top]) / scale
+            )
+        out_frame.append(out_landmark)
+    return out_frame
