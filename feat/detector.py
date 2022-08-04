Fixed scaling bug with RetinaFace and Facebox models

feat/detector.py
@@ -23,7 +23,7 @@ from feat.utils import (
 from feat.utils.io import get_resource_path
 from feat.utils.image_operations import (
     BBox,
-    extract_face,
+    extract_face_from_landmarks,
     convert_image_to_tensor,
     convert_color_vector_to_tensor,
 )
@@ -422,6 +422,7 @@ class Detector(object):
                     mean_tensor = convert_color_vector_to_tensor(mean)
                     std_tensor = convert_color_vector_to_tensor(std)
                     test_face = torch.div(torch.sub(test_face, mean_tensor), std_tensor)
+
             if concatenated_face is None:
                 concatenated_face = test_face
             else:
@@ -430,7 +431,6 @@ class Detector(object):
         # Run Landmark Model
         input = concatenated_face.type(torch.float32)
         input = torch.autograd.Variable(input)
-
         if self.info["landmark_model"]:
             if self.info["landmark_model"].lower() == "mobilefacenet":
                 landmark = self.landmark_detector(input)[0].cpu().data.numpy()
@@ -441,7 +441,7 @@ class Detector(object):
 
         landmark_results = []
         for ik in range(landmark.shape[0]):
-            landmark2 = bbox_list[ik].reproject_landmark(landmark[ik, :, :])
+            landmark2 = bbox_list[ik].inverse_transform_landmark(landmark[ik, :, :])
             landmark_results.append(landmark2)
 
         list_concat = []
@@ -544,10 +544,10 @@ class Detector(object):
 
             frame_assignment = np.where(i < lenth_cumu)[0][0]
 
-            convex_hull, new_lands = extract_face(
+            convex_hull, new_lands = extract_face_from_landmarks(
                 frame=frames[frame_assignment],
                 landmarks=flat_land[i],
-                size_output=112,
+                face_size=112,
             )
 
             hogs = hog(

feat/face_detectors/Retinaface/Retinaface_test.py
@@ -6,13 +6,14 @@ import numpy as np
 # import time
 # import feat
 from feat.face_detectors.Retinaface.Retinaface_model import PriorBox, RetinaFace
-from feat.face_detectors.Retinaface.Retinaface_utils import (
-    decode,
-    decode_landm,
-)
+from feat.face_detectors.Retinaface.Retinaface_utils import decode_landm
 from feat.utils import set_torch_device
 from feat.utils.io import get_resource_path
-from feat.utils.image_operations import convert_color_vector_to_tensor, py_cpu_nms
+from feat.utils.image_operations import (
+    convert_color_vector_to_tensor,
+    py_cpu_nms,
+    decode,
+)
 
 
 class Retinaface:
@@ -96,7 +97,7 @@ class Retinaface:
         forward function
 
         Args:
-            img: (B,C, H,W,C), B is batch number, C is channel, H is image height, and W is width
+            img: (B,C,H,W), B is batch number, C is channel, H is image height, and W is width
         """
 
         img = torch.sub(img, convert_color_vector_to_tensor(np.array([123, 117, 104])))
@@ -168,17 +169,26 @@ class Retinaface:
         # do NMS
         dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
         keep = py_cpu_nms(dets, self.nms_threshold)
-        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
         dets = dets[keep, :]
         landms = landms[keep]
 
         # keep top-K faster NMS
         dets = dets[: self.keep_top_k, :]
 
-        # filter using vis_threshold
+        # filter using vis_thres - rescale box size to be proportional to image size
+        scale_x, scale_y = (im_width / im_height, im_height / im_width)
         det_bboxes = []
         for b in dets:
             if b[4] > self.vis_threshold:
-                det_bboxes.append(b)
+                xmin, ymin, xmax, ymax, score = b
+                det_bboxes.append(
+                    [
+                        xmin * scale_x,
+                        ymin * scale_y,
+                        xmax * scale_x,
+                        ymax * scale_y,
+                        score,
+                    ]
+                )
 
         return det_bboxes