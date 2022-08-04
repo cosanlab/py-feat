Fixed scaling bug with RetinaFace and Facebox models

feat/face_detectors/FaceBoxes/FaceBoxes_test.py
@@ -31,15 +31,18 @@ import numpy as np
 # import cv2
 from feat.utils import set_torch_device
 import os
-from feat.face_detectors.FaceBoxes.FaceBoxes_model import FaceBoxesNet
-from feat.face_detectors.FaceBoxes.FaceBoxes_utils import (
-    PriorBox,
-    decode,
-    load_model,
-    nms,
-)
+from feat.face_detectors.FaceBoxes.FaceBoxes_model import FaceBoxesNet, PriorBox
+
+# from feat.face_detectors.FaceBoxes.FaceBoxes_utils import (
+#     load_model,
+#     nms,
+# )
 from feat.utils.io import get_resource_path
-from feat.utils.image_operations import convert_color_vector_to_tensor
+from feat.utils.image_operations import (
+    convert_color_vector_to_tensor,
+    decode,
+    py_cpu_nms,
+)
 
 
 class FaceBoxes:
@@ -51,10 +54,6 @@ class FaceBoxes:
         nms_threshold=0.3,
         vis_threshold=0.5,
         resize=1,
-        HEIGHT=720,
-        WIDTH=1080,
-        scale=1,
-        pretrained_path=os.path.join(get_resource_path(), "FaceBoxesProd.pth"),
         device="auto",
     ):
         self.cfg = {
@@ -70,8 +69,13 @@ class FaceBoxes:
 
         # initialize detector
         net = FaceBoxesNet(phase="test", size=None, num_classes=2)
-        self.net = load_model(net, pretrained_path=pretrained_path, device=self.device)
-        self.net.eval()
+        pretrained_dict = torch.load(
+            os.path.join(get_resource_path(), "FaceBoxesProd.pth"),
+            map_location=self.device,
+        )
+        net.load_state_dict(pretrained_dict, strict=False)
+        net = net.to(self.device)
+        self.net = net.eval()
 
         (
             self.confidence_threshold,
@@ -80,9 +84,6 @@ class FaceBoxes:
             self.nms_threshold,
             self.vis_threshold,
             self.resize,
-            self.HEIGHT,
-            self.WIDTH,
-            self.scale,
         ) = (
             confidence_threshold,
             top_k,
@@ -90,50 +91,20 @@ class FaceBoxes:
             nms_threshold,
             vis_threshold,
             resize,
-            HEIGHT,
-            WIDTH,
-            scale,
         )
 
     def __call__(self, img):
         """
         img is of shape BxCxHxW --
-        img is of shape BxHxWxC - old
         """
 
         img = torch.sub(img, convert_color_vector_to_tensor(np.array([104, 117, 123])))
 
-        # scaling to speed up - this is now done in image loader
-        # scale = 1
-        # if scale_flag:
-        #     h, w = img_raw.shape[1:3]
-        #     if h > HEIGHT:
-        #         scale = HEIGHT / h
-        #     if w * scale > WIDTH:
-        #         scale *= WIDTH / (w * scale)
-        #     # print(scale)
-        #     if scale == 1:
-        #         img_raw_scale = img_raw
-        #     else:
-        #         h_s = int(scale * h)
-        #         w_s = int(scale * w)
-        #         # print(h_s, w_s)
-        #         img_raw_scale = np.zeros((img_raw.shape[0], h_s, w_s, img_raw.shape[3]))
-        #         for i in range(img_raw.shape[0]):
-        #             img_raw_scale[i] = cv2.resize(img_raw[i, :, :, :], dsize=(w_s, h_s))
-        #         # img_raw_scale = cv2.resize(img_raw, dsize=(w_s, h_s))
-        #         # print(img_raw_scale.shape)
-
-        #     img = np.float32(img_raw_scale)
-        # else:
-        #     img = np.float32(img_raw)
-
-        # forward
-
         im_height, im_width = img.shape[-2:]
+
         scale = torch.Tensor([im_height, im_width, im_height, im_width])
         img = img.to(self.device)
-        scale_bbox = scale.to(self.device)
+        scale = scale.to(self.device)
 
         loc, conf = self.net(img)  # forward pass
 
@@ -144,25 +115,18 @@ class FaceBoxes:
                 im_width=im_width,
                 loc=loc[i],
                 conf=conf[i],
-                scale=self.scale,
-                img=img,
-                scale_bbox=scale_bbox,
+                scale=scale,
             )
             total_boxes.append(tmp_box)
 
         return total_boxes
 
-    def _calculate_boxinfo(
-        self, im_height, im_width, loc, conf, scale, img, scale_bbox
-    ):
+    def _calculate_boxinfo(self, im_height, im_width, loc, conf, scale):
 
-        priorbox = PriorBox(image_size=(im_height, im_width))
+        priorbox = PriorBox(self.cfg, image_size=(im_height, im_width))
         priors = priorbox.forward()
-        prior_data = priors.data
-        boxes = decode(loc.data.squeeze(0), prior_data, self.cfg["variance"])
-
-        boxes = boxes * scale_bbox / self.resize
-
+        boxes = decode(loc.data.squeeze(0), priors.data, self.cfg["variance"])
+        boxes = boxes * scale / self.resize
         boxes = boxes.cpu().numpy()
         scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
 
@@ -178,21 +142,26 @@ class FaceBoxes:
 
         # do NMS
         dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
-        # keep = py_cpu_nms(dets, args.nms_threshold)
-        keep = nms(dets, self.nms_threshold)
+        keep = py_cpu_nms(dets, self.nms_threshold)
         dets = dets[keep, :]
 
         # keep top-K faster NMS
         dets = dets[: self.keep_top_k, :]
 
-        # filter using vis_thres
+        # filter using vis_thres - rescale box size to be proportional to image size
+        scale_x, scale_y = (im_width / im_height, im_height / im_width)
         det_bboxes = []
         for b in dets:
             if b[4] > self.vis_threshold:
-                xmin, ymin, xmax, ymax, score = b[0], b[1], b[2], b[3], b[4]
-                w = xmax - xmin + 1
-                h = ymax - ymin + 1
-                bbox = [xmin, ymin, xmax, ymax, score]
-                det_bboxes.append(bbox)
+                xmin, ymin, xmax, ymax, score = b
+                det_bboxes.append(
+                    [
+                        xmin * scale_x,
+                        ymin * scale_y,
+                        xmax * scale_x,
+                        ymax * scale_y,
+                        score,
+                    ]
+                )
 
         return det_bboxes

feat/landmark_detectors/pfld_compressed_test.py
@@ -9,6 +9,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.init as init
+from torchvision.transforms import Compose
+from feat.transforms import Rescale
 
 
 def conv_bn(inp, oup, kernel, stride, padding=1):
@@ -118,6 +120,7 @@ class PFLDInference(nn.Module):
         """
 
     def forward(self, x):  # x: 3, 112, 112
+
         x = self.relu(self.bn1(self.conv1(x)))  # [64, 56, 56]
         # x = self.relu(self.bn2(self.conv2(x)))  # [64, 56, 56]
         x = self.relu(self.conv1_extra(self.dw_bn(self.dw_pool(x))))
@@ -164,11 +167,11 @@ class PFLDInference(nn.Module):
         return landmarks
 
 
-if __name__ == "__main__":
-    input = torch.randn(1, 3, 112, 112)
-    plfd_backbone = PFLDInference()
-    angle, landmarks = plfd_backbone(input)
-    print(plfd_backbone)
-    print(
-        "angle.shape:{0:}, landmarks.shape: {1:}".format(angle.shape, landmarks.shape)
-    )
+# if __name__ == "__main__":
+#     input = torch.randn(1, 3, 112, 112)
+#     plfd_backbone = PFLDInference()
+#     angle, landmarks = plfd_backbone(input)
+#     print(plfd_backbone)
+#     print(
+#         "angle.shape:{0:}, landmarks.shape: {1:}".format(angle.shape, landmarks.shape)
+#     )

feat/utils/image_operations.py
@@ -33,6 +33,7 @@ __all__ = [
     "mask_image",
     "convert_to_euler",
     "py_cpu_nms",
+    "decode",
 ]
 
 # Neutral face coordinates
@@ -98,7 +99,7 @@ def registration(face_lms, neutral=neutral, method="fullface"):
     return np.array(registered_lms)
 
 
-def extract_face(frame, landmarks, size_output=112):
+def extract_face_from_landmarks(frame, landmarks, face_size=112):
     """Extract a face in a frame with a convex hull of landmarks.
 
     This function extracts the faces of the frame with convex hulls and masks out the rest.
@@ -124,7 +125,7 @@ def extract_face(frame, landmarks, size_output=112):
     landmarks = np.array(landmarks).copy()
 
     aligned_img, new_landmarks = align_face_68pts(
-        frame, landmarks.flatten(), 2.5, img_size=size_output
+        frame, landmarks.flatten(), 2.5, img_size=face_size
     )
 
     hull = ConvexHull(new_landmarks)
@@ -394,12 +395,25 @@ def align_face_49pts(img, img_land, box_enlarge=2.9, img_size=200):
 
 
 class BBox(object):
-    def __init__(self, bbox, order=None):
+    def __init__(
+        self,
+        bbox,
+        order=None,
+        left_boundary=0,
+        top_boundary=0,
+        right_boundary=None,
+        bottom_boundary=None,
+    ):
         """Class to work with Bounding Box
 
         Args:
             bbox: (list): values
             order (list): order of values (e.g., ['left', 'bottom', 'right', 'top'])
+            left optional (float): boundary of left (default 0)
+            right toptional (float): boundary of right border (e.g., width of image)
+            top optional (float): boundary of top border (default 0)
+            bottom optional(float): boundary of right border (e.g., height of image)
+
         """
         if order is None:
             self.order = ["left", "bottom", "right", "top"]
@@ -420,33 +434,94 @@ class BBox(object):
         self.width = self.right - self.left
         self.height = self.top - self.bottom
 
+        # self.set_boundary(
+        #     left=left_boundary,
+        #     right=right_boundary,
+        #     top=top_boundary,
+        #     bottom=bottom_boundary,
+        #     apply=True,
+        # )
+
     def __repr__(self):
         return f"'height': {self.height}, 'width': {self.width}"
 
-    def expand_by_factor(self, factor):
+    def expand_by_factor(self, factor, symmetric=True):
         """Expand box by factor
 
         Args:
-            factor (float, tuple): factor to expand. if float equal in all directions, if tuple (height, width)
+            factor (float): factor to expand.
+            symmetric (bool): if symmetric then expand equally based on largest side
         """
 
-        if not isinstance(factor, (int, float, tuple)):
-            raise ValueError("factor must be float or tuple(height, width)")
+        if symmetric:
+            new_size = max([self.width, self.height]) * factor
+            self.width = new_size
+            self.height = new_size
 
-        if isinstance(factor, tuple):
-            if len(factor) != 2:
-                raise ValueError("factor must include (height,width)")
-            else:
-                self.height *= factor[0]
-                self.width *= factor[1]
         else:
-            self.height *= factor
             self.width *= factor
-        self.left = max(self.center_x - self.width, 0)
-        self.right = self.center_x + self.width
-        self.bottom = max(self.center_x - self.height, 0)
-        self.top = self.center_x + self.height
+            self.height *= factor
+
+        self.left = self.center_x - (self.width // 2)
+        self.right = self.center_x + (self.width // 2)
+        self.bottom = self.center_y - (self.height // 2)
+        self.top = self.center_y + (self.height // 2)
+
+        # self._apply_boundary()
+
         return self
+
+    def set_boundary(self, left=0, right=None, top=0, bottom=None, apply_boundary=True):
+        """Set maximum boundary of bounding box such as the edge of the original image
+
+        Use _apply_boundary() method to update the bounding box
+
+        Args:
+            left (float): boundary of left (default 0)
+            right (float): boundary of right border (e.g., width of image)
+            top (float): boundary of top border (default 0)
+            bottom (float): boundary of right border (e.g., height of image)
+            apply (bool): apply boundary to BBox
+
+        """
+
+        left = max(left, 0)
+        top = max(top, 0)
+
+        (
+            self.left_boundary,
+            self.top_boundary,
+            self.right_boundary,
+            self.bottom_boundary,
+        ) = (left, top, right, bottom)
+
+        if apply_boundary:
+            self._apply_boundary()
+        return self
+
+    def _apply_boundary(self):
+        """Helper function to apply stored boundaries to BBox
+
+        Currently does not update stored width and height
+        """
+
+        if self.left_boundary is not None:
+            if self.left_boundary > self.left:
+                self.left = self.left_boundary
+
+        if self.right_boundary is not None:
+            if self.right_boundary < self.right:
+                self.right = self.right_boundary
+
+        if self.top_boundary is not None:
+            if self.top_boundary > self.top:
+                self.top = self.top_boundary
+
+        if self.bottom_boundary is not None:
+            if self.bottom_boundary < self.bottom:
+                self.bottom = self.bottom_boundary
+
+        return
 
     def extract_from_image(self, img):
         """Crop Image using Bounding Box
@@ -488,8 +563,17 @@ class BBox(object):
         """Output bounding box coordinates to list"""
         return [self.to_dict()[x] for x in self.order]
 
-    def project_landmark(self, landmark):
-        """scale to [0,1] from https://github.com/cunjian/pytorch_face_landmark/"""
+    def transform_landmark(self, landmark):
+        """Scale Landmarks to be within a 1 unit box (e.g., [0,1])
+
+        based on https://github.com/cunjian/pytorch_face_landmark/
+
+        Args:
+
+        Returns:
+            scaled landmarks
+        """
+
         landmark_ = np.asarray(np.zeros(landmark.shape))
         for i, point in enumerate(landmark):
             landmark_[i] = (
@@ -498,8 +582,18 @@ class BBox(object):
             )
         return landmark_
 
-    def reproject_landmark(self, landmark):
-        """landmark of (5L, 2L) from [0,1] to real range from https://github.com/cunjian/pytorch_face_landmark/"""
+    def inverse_transform_landmark(self, landmark):
+        """Re-scale landmarks from unit scaling back into BBox
+
+        based on  https://github.com/cunjian/pytorch_face_landmark/
+
+        Args:
+            landmarks: (np.array): landmarks
+
+        Returns:
+            re-scaled landmarks
+        """
+
         landmark_ = np.asarray(np.zeros(landmark.shape))
         for i, point in enumerate(landmark):
             x = point[0] * self.width + self.left
@@ -653,3 +747,32 @@ def py_cpu_nms(dets, thresh):
         order = order[inds + 1]
 
     return keep
+
+
+def decode(loc, priors, variances):
+    """Decode locations from predictions using priors to undo
+    the encoding we did for offset regression at train time.
+
+    Adapted from https://github.com/Hakuyume/chainer-ssd
+
+    Args:
+        loc (tensor): location predictions for loc layers,
+            Shape: [num_priors,4]
+        priors (tensor): Prior boxes in center-offset form.
+            Shape: [num_priors,4].
+        variances: (list[float]) Variances of priorboxes
+
+    Return:
+        decoded bounding box predictions
+    """
+
+    boxes = torch.cat(
+        (
+            priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:],
+            priors[:, 2:] * torch.exp(loc[:, 2:] * variances[1]),
+        ),
+        1,
+    )
+    boxes[:, :2] -= boxes[:, 2:] / 2
+    boxes[:, 2:] += boxes[:, :2]
+    return boxes