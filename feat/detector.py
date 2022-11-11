de3d0b84108ac037101127f9e45abab44c098d94
ejolly <eshin.jolly@gmail.com>
2022-11-11 17:45:22 -0500

    change img2pose to make detector API more consistent

--- feat/detector.py
+++ feat/detector.py
@@ -423,13 +423,11 @@ class Detector(object):
         """
 
         logging.info("detecting poses...")
+
         # Normalize Data
         frame = convert_image_to_tensor(frame, img_type="float32") / 255
 
-        if "img2pose" in self.info["facepose_model"]:
-            faces, poses = self.facepose_detector(frame)
-        else:
-            poses = self.facepose_detector(frame, landmarks)
+        poses = self.facepose_detector(frame, landmarks)
 
         return poses
 
