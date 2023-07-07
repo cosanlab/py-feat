b1021db30aca65809aae8c356db9fa08c46e35fb
Tiankang Xie <tiankang@liquidswords.dartmouth.edu>
2023-07-07 14:30:30 -0400

    formatting

--- feat/au_detectors/StatLearning/SL_test.py
+++ feat/au_detectors/StatLearning/SL_test.py
@@ -21,12 +21,24 @@ def load_classifier_pkl(cf_path):
 
 class SVMClassifier:
     def __init__(self) -> None:
-        self.scaler_upper, self.pca_model_upper = load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Upperscalar_June30.pkl")), load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Upperpca_June30.pkl"))
-        
-        self.scaler_lower, self.pca_model_lower = load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Lowerscalar_June30.pkl")), load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Lowerpca_June30.pkl"))
-        
-        self.scaler_full, self.pca_model_full = load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Fullscalar_June30.pkl")), load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Fullpca_June30.pkl"))
-        
+        self.scaler_upper, self.pca_model_upper = load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Upperscalar_June30.pkl")
+        ), load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Upperpca_June30.pkl")
+        )
+
+        self.scaler_lower, self.pca_model_lower = load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Lowerscalar_June30.pkl")
+        ), load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Lowerpca_June30.pkl")
+        )
+
+        self.scaler_full, self.pca_model_full = load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Fullscalar_June30.pkl")
+        ), load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Fullpca_June30.pkl")
+        )
+
         self.classifier = load_classifier(
             os.path.join(get_resource_path(), "svm_60_July2023.pkl")
         )
@@ -58,11 +70,26 @@ class SVMClassifier:
 
         pred_aus = []
         for keys in aus_list:
-            if keys in ["AU1","AU4","AU6"]:
+            if keys in ["AU1", "AU4", "AU6"]:
                 au_pred = self.classifier[keys].predict(pca_transformed_upper)
-            elif keys in ["AU11", "AU12","AU17"]:
+            elif keys in ["AU11", "AU12", "AU17"]:
                 au_pred = self.classifier[keys].predict(pca_transformed_lower)
-            elif keys in ["AU2","AU5","AU7","AU9","AU10","AU14","AU15","AU20","AU23","AU24","AU25","AU26","AU28","AU43"]:
+            elif keys in [
+                "AU2",
+                "AU5",
+                "AU7",
+                "AU9",
+                "AU10",
+                "AU14",
+                "AU15",
+                "AU20",
+                "AU23",
+                "AU24",
+                "AU25",
+                "AU26",
+                "AU28",
+                "AU43",
+            ]:
                 au_pred = self.classifier[keys].predict(pca_transformed_full)
             else:
                 raise ValueError("unknown AU detected")
@@ -74,13 +101,24 @@ class SVMClassifier:
 
 class XGBClassifier:
     def __init__(self) -> None:
-        
-        self.scaler_upper, self.pca_model_upper = load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Upperscalar_June30.pkl")), load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Upperpca_June30.pkl"))
-        
-        self.scaler_lower, self.pca_model_lower = load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Lowerscalar_June30.pkl")), load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Lowerpca_June30.pkl"))
-        
-        self.scaler_full, self.pca_model_full = load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Fullscalar_June30.pkl")), load_classifier_pkl(os.path.join(get_resource_path(), "all_data_Fullpca_June30.pkl"))
-        
+        self.scaler_upper, self.pca_model_upper = load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Upperscalar_June30.pkl")
+        ), load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Upperpca_June30.pkl")
+        )
+
+        self.scaler_lower, self.pca_model_lower = load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Lowerscalar_June30.pkl")
+        ), load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Lowerpca_June30.pkl")
+        )
+
+        self.scaler_full, self.pca_model_full = load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Fullscalar_June30.pkl")
+        ), load_classifier_pkl(
+            os.path.join(get_resource_path(), "all_data_Fullpca_June30.pkl")
+        )
+
         self.au_keys = [
             "AU1",
             "AU2",
@@ -130,21 +168,32 @@ class XGBClassifier:
 
         pred_aus = []
         for keys in self.au_keys:
-            
             classifier = xgb.XGBClassifier()
             classifier.load_model(
                 os.path.join(get_resource_path(), f"July4_{keys}_XGB.ubj")
             )
-        
-            if keys in ["AU1","AU2","AU7"]:
+
+            if keys in ["AU1", "AU2", "AU7"]:
                 au_pred = classifier.predict_proba(pca_transformed_upper)[:, 1]
-            elif keys in ["AU11","AU14","AU17","AU23","AU24","AU26"]:
+            elif keys in ["AU11", "AU14", "AU17", "AU23", "AU24", "AU26"]:
                 au_pred = classifier.predict_proba(pca_transformed_lower)[:, 1]
-            elif keys in ["AU4","AU5","AU6","AU9","AU10","AU12","AU15","AU20","AU25","AU28","AU43"]:
+            elif keys in [
+                "AU4",
+                "AU5",
+                "AU6",
+                "AU9",
+                "AU10",
+                "AU12",
+                "AU15",
+                "AU20",
+                "AU25",
+                "AU28",
+                "AU43",
+            ]:
                 au_pred = classifier.predict_proba(pca_transformed_full)[:, 1]
             else:
                 raise ValueError("unknown AU detected")
-        
+
             # au_pred = au_pred[:, 1]
             pred_aus.append(au_pred)
 
