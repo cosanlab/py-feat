14030cb9a3b8bbdc6e65f868aa4ce6145b43202c
ejolly <eshin.jolly@gmail.com>
2023-09-20 21:53:15 -0400

    rename method to iplot and retain old plotting method

--- feat/data.py
+++ feat/data.py
@@ -350,6 +350,10 @@ class FexSeries(Series):
         """Alias for Fex.plot_detections"""
         return Fex(self).T.__finalize__(self).plot_detections(*args, **kwargs)
 
+    def iplot_detections(self, *args, **kwargs):
+        """Alias for Fex.iplot_detections"""
+        return Fex(self).T.__finalize__(self).iplot_detections(*args, **kwargs)
+
 
 # TODO: Switch all print statements to respect verbose
 class Fex(DataFrame):
@@ -2285,7 +2289,7 @@ class Fex(DataFrame):
 
         return fig
 
-    def plot_detections(
+    def iplot_detections(
         self,
         faceboxes=True,
         landmarks=True,
@@ -2385,197 +2389,195 @@ class Fex(DataFrame):
 
         return fig
 
-        # Deprecated original plot_detections plot using matplotlib backend
+    def plot_detections(
+        self,
+        faces="landmarks",
+        faceboxes=True,
+        muscles=False,
+        poses=False,
+        gazes=False,
+        add_titles=True,
+        au_barplot=True,
+        emotion_barplot=True,
+        plot_original_image=True,
+    ):
+        """
+        Plots detection results by Feat. Can control plotting of face, AU barplot and
+        Emotion barplot. The faces kwarg controls whether facial landmarks are draw on
+        top of input images or whether faces are visualized using Py-Feat's AU
+        visualization model using detected AUs. If detection was performed on a video an
+        faces = 'landmarks', only an outline of the face will be draw without loading
+        the underlying vidoe frame to save memory.
 
-        # def plot_detections(
-        #     self,
-        #     faces="landmarks",
-        #     faceboxes=True,
-        #     muscles=False,
-        #     poses=False,
-        #     gazes=False,
-        #     add_titles=True,
-        #     au_barplot=True,
-        #     emotion_barplot=True,
-        #     plot_original_image=True,
-        # ):
-        #     """
-        #     Plots detection results by Feat. Can control plotting of face, AU barplot and
-        #     Emotion barplot. The faces kwarg controls whether facial landmarks are draw on
-        #     top of input images or whether faces are visualized using Py-Feat's AU
-        #     visualization model using detected AUs. If detection was performed on a video an
-        #     faces = 'landmarks', only an outline of the face will be draw without loading
-        #     the underlying vidoe frame to save memory.
+        Args:
+            faces (str, optional): 'landmarks' to draw detected landmarks or 'aus' to
+            generate a face from AU detections using Py-Feat's AU landmark model.
+            Defaults to 'landmarks'.
+            faceboxes (bool, optional): Whether to draw the bounding box around detected
+            faces. Only applies if faces='landmarks'. Defaults to True.
+            muscles (bool, optional): Whether to draw muscles from AU activity. Only
+            applies if faces='aus'. Defaults to False.
+            poses (bool, optional): Whether to draw facial poses. Only applies if
+            faces='landmarks'. Defaults to False.
+            gazes (bool, optional): Whether to draw gaze vectors. Only applies if faces='aus'. Defaults to False.
+            add_titles (bool, optional): Whether to add the file name as a title above
+            the face. Defaults to True.
+            au_barplot (bool, optional): Whether to include a subplot for au detections. Defaults to True.
+            emotion_barplot (bool, optional): Whether to include a subplot for emotion detections. Defaults to True.
 
-        #     Args:
-        #         faces (str, optional): 'landmarks' to draw detected landmarks or 'aus' to
-        #         generate a face from AU detections using Py-Feat's AU landmark model.
-        #         Defaults to 'landmarks'.
-        #         faceboxes (bool, optional): Whether to draw the bounding box around detected
-        #         faces. Only applies if faces='landmarks'. Defaults to True.
-        #         muscles (bool, optional): Whether to draw muscles from AU activity. Only
-        #         applies if faces='aus'. Defaults to False.
-        #         poses (bool, optional): Whether to draw facial poses. Only applies if
-        #         faces='landmarks'. Defaults to False.
-        #         gazes (bool, optional): Whether to draw gaze vectors. Only applies if faces='aus'. Defaults to False.
-        #         add_titles (bool, optional): Whether to add the file name as a title above
-        #         the face. Defaults to True.
-        #         au_barplot (bool, optional): Whether to include a subplot for au detections. Defaults to True.
-        #         emotion_barplot (bool, optional): Whether to include a subplot for emotion detections. Defaults to True.
+        Returns:
+            list: list of matplotlib figures
+        """
 
-        #     Returns:
-        #         list: list of matplotlib figures
-        #     """
+        # Plotting logic, eventually refactor me!:
+        # Possible detections:
+        # 1. Single image - single-face
+        # 2. Single image - multi-face
+        # 3. Multi image - single-face per image
+        # 4. Multi image - multi-face per image
+        # 5. Multi image - single and multi-face mix per image
+        # 6. Video - single-face for all frames
+        # 7. Video - multi-face for all frames
+        # 8. Video - single and multi-face mix across frames
 
-        #     # Plotting logic, eventually refactor me!:
-        #     # Possible detections:
-        #     # 1. Single image - single-face
-        #     # 2. Single image - multi-face
-        #     # 3. Multi image - single-face per image
-        #     # 4. Multi image - multi-face per image
-        #     # 5. Multi image - single and multi-face mix per image
-        #     # 6. Video - single-face for all frames
-        #     # 7. Video - multi-face for all frames
-        #     # 8. Video - single and multi-face mix across frames
+        sns.set_context("paper", font_scale=2.0)
 
-        #     sns.set_context("paper", font_scale=2.0)
+        num_subplots = bool(faces) + au_barplot + emotion_barplot
 
-        #     num_subplots = bool(faces) + au_barplot + emotion_barplot
+        all_figs = []
+        for _, frame in enumerate(self.frame.unique()):
+            # Determine figure width based on how many subplots we have
+            f = plt.figure(figsize=(5 * num_subplots, 7))
+            spec = f.add_gridspec(ncols=num_subplots, nrows=1)
+            col_count = 0
+            plot_data = self.query("frame == @frame")
+            face_ax, au_ax, emo_ax = None, None, None
+            if faces is not False:
+                face_ax = f.add_subplot(spec[0, col_count])
+                col_count += 1
+            if au_barplot:
+                au_ax = f.add_subplot(spec[0, col_count])
+                col_count += 1
+            if emotion_barplot:
+                emo_ax = f.add_subplot(spec[0, col_count])
+                col_count += 1
 
-        #     all_figs = []
-        #     for _, frame in enumerate(self.frame.unique()):
-        #         # Determine figure width based on how many subplots we have
-        #         f = plt.figure(figsize=(5 * num_subplots, 7))
-        #         spec = f.add_gridspec(ncols=num_subplots, nrows=1)
-        #         col_count = 0
-        #         plot_data = self.query("frame == @frame")
-        #         face_ax, au_ax, emo_ax = None, None, None
-        #         if faces is not False:
-        #             face_ax = f.add_subplot(spec[0, col_count])
-        #             col_count += 1
-        #         if au_barplot:
-        #             au_ax = f.add_subplot(spec[0, col_count])
-        #             col_count += 1
-        #         if emotion_barplot:
-        #             emo_ax = f.add_subplot(spec[0, col_count])
-        #             col_count += 1
+            for _, row in plot_data.iterrows():
+                # DRAW LANDMARKS ON IMAGE OR AU FACE
+                if face_ax is not None:
+                    facebox = row[self.facebox_columns].values
 
-        #         for _, row in plot_data.iterrows():
-        #             # DRAW LANDMARKS ON IMAGE OR AU FACE
-        #             if face_ax is not None:
-        #                 facebox = row[self.facebox_columns].values
+                    if not faces == "aus" and plot_original_image:
+                        file_extension = os.path.basename(row["input"]).split(".")[-1]
+                        if file_extension.lower() in [
+                            "jpg",
+                            "jpeg",
+                            "png",
+                            "bmp",
+                            "tiff",
+                            "pdf",
+                        ]:
+                            img = read_image(row["input"])
+                        else:
+                            # Ignore UserWarning: The pts_unit 'pts' gives wrong results. Please use
+                            # pts_unit 'sec'. See why it's ok in this issue:
+                            # https://github.com/pytorch/vision/issues/1931
+                            with warnings.catch_warnings():
+                                warnings.simplefilter("ignore", UserWarning)
+                                video, audio, info = read_video(
+                                    row["input"], output_format="TCHW"
+                                )
+                            img = video[row["frame"], :, :]
+                        color = "w"
+                        face_ax.imshow(img.permute([1, 2, 0]))
+                    else:
+                        color = "k"  # drawing lineface but not on photo
 
-        #                 if not faces == "aus" and plot_original_image:
-        #                     file_extension = os.path.basename(row["input"]).split(".")[-1]
-        #                     if file_extension.lower() in [
-        #                         "jpg",
-        #                         "jpeg",
-        #                         "png",
-        #                         "bmp",
-        #                         "tiff",
-        #                         "pdf",
-        #                     ]:
-        #                         img = read_image(row["input"])
-        #                     else:
-        #                         # Ignore UserWarning: The pts_unit 'pts' gives wrong results. Please use
-        #                         # pts_unit 'sec'. See why it's ok in this issue:
-        #                         # https://github.com/pytorch/vision/issues/1931
-        #                         with warnings.catch_warnings():
-        #                             warnings.simplefilter("ignore", UserWarning)
-        #                             video, audio, info = read_video(
-        #                                 row["input"], output_format="TCHW"
-        #                             )
-        #                         img = video[row["frame"], :, :]
-        #                     color = "w"
-        #                     face_ax.imshow(img.permute([1, 2, 0]))
-        #                 else:
-        #                     color = "k"  # drawing lineface but not on photo
+                    if faceboxes:
+                        rect = Rectangle(
+                            (facebox[0], facebox[1]),
+                            facebox[2],
+                            facebox[3],
+                            linewidth=2,
+                            edgecolor="cyan",
+                            fill=False,
+                        )
+                        face_ax.add_patch(rect)
 
-        #                 if faceboxes:
-        #                     rect = Rectangle(
-        #                         (facebox[0], facebox[1]),
-        #                         facebox[2],
-        #                         facebox[3],
-        #                         linewidth=2,
-        #                         edgecolor="cyan",
-        #                         fill=False,
-        #                     )
-        #                     face_ax.add_patch(rect)
+                    if poses:
+                        face_ax = draw_facepose(
+                            pose=row[self.facepose_columns].values,
+                            facebox=facebox,
+                            ax=face_ax,
+                        )
 
-        #                 if poses:
-        #                     face_ax = draw_facepose(
-        #                         pose=row[self.facepose_columns].values,
-        #                         facebox=facebox,
-        #                         ax=face_ax,
-        #                     )
+                    if faces == "landmarks":
+                        landmark = row[self.landmark_columns].values
+                        currx = landmark[:68]
+                        curry = landmark[68:]
 
-        #                 if faces == "landmarks":
-        #                     landmark = row[self.landmark_columns].values
-        #                     currx = landmark[:68]
-        #                     curry = landmark[68:]
+                        # facelines
+                        face_ax = draw_lineface(
+                            currx, curry, ax=face_ax, color=color, linewidth=3
+                        )
+                        if not plot_original_image:
+                            face_ax.invert_yaxis()
 
-        #                     # facelines
-        #                     face_ax = draw_lineface(
-        #                         currx, curry, ax=face_ax, color=color, linewidth=3
-        #                     )
-        #                     if not plot_original_image:
-        #                         face_ax.invert_yaxis()
+                    elif faces == "aus":
+                        # Generate face from AU landmark model
+                        if any(self.groupby("frame").size() > 1):
+                            raise NotImplementedError(
+                                "Plotting using AU landmark model is not currently supported for detections that contain multiple faces"
+                            )
+                        if muscles:
+                            muscles = {"all": "heatmap"}
+                        else:
+                            muscles = {}
+                        aus, gaze, muscles, model = self._prepare_plot_aus(
+                            row, muscles=muscles, gaze=gazes
+                        )
+                        title = row["input"] if add_titles else None
+                        face_ax = plot_face(
+                            model=model,
+                            au=aus,
+                            gaze=gaze,
+                            ax=face_ax,
+                            muscles=muscles,
+                            title=None,
+                        )
+                    else:
+                        raise ValueError(
+                            f"faces={type(faces)} is not currently supported try ['False','landmarks','aus']."
+                        )
 
-        #                 elif faces == "aus":
-        #                     # Generate face from AU landmark model
-        #                     if any(self.groupby("frame").size() > 1):
-        #                         raise NotImplementedError(
-        #                             "Plotting using AU landmark model is not currently supported for detections that contain multiple faces"
-        #                         )
-        #                     if muscles:
-        #                         muscles = {"all": "heatmap"}
-        #                     else:
-        #                         muscles = {}
-        #                     aus, gaze, muscles, model = self._prepare_plot_aus(
-        #                         row, muscles=muscles, gaze=gazes
-        #                     )
-        #                     title = row["input"] if add_titles else None
-        #                     face_ax = plot_face(
-        #                         model=model,
-        #                         au=aus,
-        #                         gaze=gaze,
-        #                         ax=face_ax,
-        #                         muscles=muscles,
-        #                         title=None,
-        #                     )
-        #                 else:
-        #                     raise ValueError(
-        #                         f"faces={type(faces)} is not currently supported try ['False','landmarks','aus']."
-        #                     )
+                    if add_titles:
+                        _ = face_ax.set_title(
+                            "\n".join(wrap(os.path.basename(row["input"]))),
+                            loc="center",
+                            wrap=True,
+                            fontsize=14,
+                        )
 
-        #                 if add_titles:
-        #                     _ = face_ax.set_title(
-        #                         "\n".join(wrap(os.path.basename(row["input"]))),
-        #                         loc="center",
-        #                         wrap=True,
-        #                         fontsize=14,
-        #                     )
+                    face_ax.axes.get_xaxis().set_visible(False)
+                    face_ax.axes.get_yaxis().set_visible(False)
 
-        #                 face_ax.axes.get_xaxis().set_visible(False)
-        #                 face_ax.axes.get_yaxis().set_visible(False)
+            # DRAW AU BARPLOT
+            if au_ax is not None:
+                _ = plot_data.aus.T.plot(kind="barh", ax=au_ax)
+                _ = au_ax.invert_yaxis()
+                _ = au_ax.legend().remove()
+                _ = au_ax.set(xlim=[0, 1.1], title="Action Units")
 
-        #         # DRAW AU BARPLOT
-        #         if au_ax is not None:
-        #             _ = plot_data.aus.T.plot(kind="barh", ax=au_ax)
-        #             _ = au_ax.invert_yaxis()
-        #             _ = au_ax.legend().remove()
-        #             _ = au_ax.set(xlim=[0, 1.1], title="Action Units")
+            # DRAW EMOTION BARPLOT
+            if emo_ax is not None:
+                _ = plot_data.emotions.T.plot(kind="barh", ax=emo_ax)
+                _ = emo_ax.invert_yaxis()
+                _ = emo_ax.legend().remove()
+                _ = emo_ax.set(xlim=[0, 1.1], title="Emotions")
 
-        #         # DRAW EMOTION BARPLOT
-        #         if emo_ax is not None:
-        #             _ = plot_data.emotions.T.plot(kind="barh", ax=emo_ax)
-        #             _ = emo_ax.invert_yaxis()
-        #             _ = emo_ax.legend().remove()
-        #             _ = emo_ax.set(xlim=[0, 1.1], title="Emotions")
-
-        #         f.tight_layout()
-        #         all_figs.append(f)
-        #     return all_figs
+            f.tight_layout()
+            all_figs.append(f)
+        return all_figs
 
         # TODO: turn this into a property using a @property and @sessions.settr decorators
         # Tried it but was running into maximum recursion depth errors. Maybe some
