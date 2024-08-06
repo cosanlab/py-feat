f69832e21a263e065363bb46c49c745eaa22ed64
ejolly <eshin.jolly@gmail.com>
2024-08-06 11:50:09 -0400

    start adding basic tests

--- feat/tests/test_fast_detector.py
+++ feat/tests/test_fast_detector.py
@@ -4,11 +4,13 @@ from feat.FastDetector import FastDetector
 from feat.data import Fex
 from huggingface_hub import PyTorchModelHubMixin
 
+
 @pytest.mark.usefixtures(
     "single_face_img", "single_face_img_data", "multi_face_img", "multi_face_img_data"
 )
 class Test_Fast_Detector:
     """Test new single model detector"""
+
     detector = FastDetector(device="cpu")
 
     def test_init(self):
@@ -17,3 +19,19 @@ class Test_Fast_Detector:
     def test_detect_image(self, single_face_img):
         fex = self.detector.detect_image(single_face_img)
         assert isinstance(fex, Fex)
+
+        # No bad predictions on default image
+        assert not fex.isnull().any().any()
+
+        # Default output is 686 features
+        assert fex.shape == (1, 686)
+
+        # Bounding box
+        assert 180 < fex.FaceRectX[0] < 200
+        assert 140 < fex.FaceRectY[0] < 160
+
+        # Jin is smiling
+        assert fex.happiness[0] > 0.8
+
+        # AU checks; TODO: Add more
+        assert fex.aus.AU20[0] > 0.8
